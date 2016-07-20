4869ebad2ee90ea7cd86a48d12077f24300d8ef3
pcarrasco <pcarrasco015@gmail.com>
2016-07-20 02:56:47 -0400

    Added missing file

---
+++ FootlooseFS.DataPersistence/SQL/FootlooseFSSQLLocalDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootlooseFS.DataPersistence
+{
+    /// <summary>
+    /// DB Context for SQL LocalDB connection
+    /// </summary>
+    public class FootlooseFSSQLLocalDBContext : FootlooseFSDBContext
+    {
+        public FootlooseFSSQLLocalDBContext() : base()
+        {
+            // Setup the data directory for the SQL Server LocalDB connection which references 
+            // DataDirectory in the path of the MDF file
+            var dataDirectory = ConfigurationManager.AppSettings["DataDirectory"];
+            AppDomain.CurrentDomain.SetData("DataDirectory", dataDirectory);
+        }
+    }
+}
